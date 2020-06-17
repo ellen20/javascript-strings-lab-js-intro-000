fefed68c2ecefd1dfc23e01a9caa9d1fec3ec6c0
jingling jin <jljin0524@gmail.com>
2020-06-17 01:59:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-var greeting = "Hello, everybody!";
+var greeting = "";
 
 var specialGuest = "Neil deGrasse Tyson"
 
-var greetSpecialGuest = `"Hello," + ${specialGuest} + "!"`;
+var greetSpecialGuest = "Hello," + specialGuest + "!";
 
 var topic = "space";
 
